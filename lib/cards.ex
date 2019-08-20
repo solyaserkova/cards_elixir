6ca65b6ca8b1404ecd11eaaca9a2b6b51c34f1a9
Olya <solyaserkova@gmail.com>
2019-08-20 11:13:51 +0300

    first commit

---
+++ lib/cards.ex
@@ -0,0 +1,31 @@
+defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # nested comprehasion
+    # cards =
+    #   for value <- values do
+    #     for suit <- suits do
+    #       "#{value} of #{suit}"
+    #     end
+    #   end
+    # List.flatten(cards)
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+end
